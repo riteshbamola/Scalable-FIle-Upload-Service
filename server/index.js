49f9ccd3f9407c668d2d6686fa752b54c6ee2706
Ritesh Bamola <rishubamola01@gmail.com>
2026-03-07 19:21:50 +0530

    dockerize backend

--- server/index.js
+++ server/index.js
@@ -32,6 +32,6 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
